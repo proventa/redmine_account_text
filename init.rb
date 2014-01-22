09a66ca13facd2695fede43075a40b4476475e64
Clemens Bergmann <c.bergmann@proventa.de>
2014-01-22 10:48:50 +0100

    inital commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_account_text do
+  name 'Redmine Account Text plugin'
+  author 'Clemens Bergmann'
+  description 'This plugin show a text on the account page'
+  version '0.0.1'
+  url 'http://github.com/proventa/redmine_account_text'
+  author_url 'http://proventa.de'
+end
